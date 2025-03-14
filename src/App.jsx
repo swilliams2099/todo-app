e7d54387ee433175c4e75e491185dff56eb30824
S <s@Ss-MacBook-Air.local>
2025-03-14 15:28:02 -0700

    add features to mark as completed, edit, delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,91 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  // here?
-  const taskList = props.tasks?.map((task) => (
+
+  const [tasks, setTasks] = useState(props.tasks); // returns 2-item array
+  // tasks = props.tasks
+  // setTasks() changes tasks
+
+  // CALLBACK FUNCTIONS
+  // 1. ADD TASK
+  function addTask(name) {
+    // alert(name);
+    // TODO add task to taskList with setTasks()
+    // make newTask object with parameters id, name, completed,
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    // taskList2 = [...taskList1];
+    // taskList2.push(newTask);
+    setTasks([...tasks, newTask]);
+    console.log(tasks);
+  }
+
+  // 2. TOGGLE COMPLETED
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted (reassigned)
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  // 3. DELETE TASK
+  function deleteTask(id) {
+    // console.log(id);
+    // const updatedTasks = tasks.map((task) => {
+    //   if (id === task.id) {
+
+    //   }
+    const remainingTasks = tasks.filter((task) => task.id !== id);
+    setTasks(remainingTasks);
+  }
+
+  // 4. EDIT TASK
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return {...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  // RENDERING (?)
+
+  const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       complete={task.completed}
-      key={task.id} //reserved word
+      key={task.id} // key is a reserved word
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
     />));
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1 hidden={false}>TodoMatic</h1>
-      <Form />
+      {/* same name "addTask" is a coincidence; try onAddTask */}
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,47 @@
-function Form() {
+import { useState } from "react";
+
+function Form(props) {
+
+    // initial state
+    const [name, setName] = useState(""); // useState() returns an array of 2 items
+    // 0: current value of state
+    // 1: function used to update state
+    // ->
+    // name = ""
+    // setName() is a function to modify name
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (name != "") {
+            props.addTask(name); //callback function
+        }
+        setName(""); // Reset 
+    }
+
+    function handleChange(event) {
+        setName(event.target.value);
+    }
+
     return (
-        <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+        <form onSubmit={handleSubmit}>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input" className="label__lg">
+                    What needs to be done?
+                </label>
+            </h2>
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input__lg"
+                name="text"
+                autoComplete="off"
+                value={name}
+                onChange={handleChange}
+            />
+            <button type="submit" className="btn btn__primary btn__lg">
+                Add
+            </button>
+        </form>
     )
 
 }
